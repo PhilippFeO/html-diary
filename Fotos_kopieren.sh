a76b9570b54db25ff7a7d7cad468e56c36a710eb
Philipp Rost <rost.philipp@protonmail.com>
2023-04-28 16:33:55 +0200

    Hard link instead of copy

--- Fotos_kopieren.sh
+++ Fotos_kopieren.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
+# nemo script for hard linking selected fotos into the according diary folder
 
 today=$(date +%F)
 month_nmb=$(date +%m)
 month_name=$(date +%B)
 year=$(date +%Y)
 
-# Create directory for today
 path=~/.tagebuch/$year/$month_nmb-$month_name/$today
 
+# If directory exists, hard link marked images into it
 for img in $NEMO_SCRIPT_SELECTED_FILE_PATHS; do
-	cp $img $path
-	echo $img
+	ln $img $path
 done
 
 nemo --quit
