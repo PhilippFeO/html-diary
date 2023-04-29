9cde8f7a576d23976acf318f2384293a543c3f3b
Philipp Rost <rost.philipp@protonmail.com>
2023-04-29 16:03:24 +0200

    Open past of current day

--- ago.sh
+++ ago.sh
@@ -3,29 +3,56 @@
 # Past dates are specified in past_dates.txt
 
 
-past_dates=( $(date --file=past_dates.txt +%F) )
+zenity --question --text="In die Vergangenheit blicken?" --ok-label="Ja" --cancel-label="Nein" --timeout=10
 
-for past_date in ${past_dates[@]}; do
-	today_past=$(date --date="$past_date" "+%F-%A")
-	month_past=$(date --date="$past_date" "+%m-%B")
-	year_past=$(date --date="$past_date" "+%Y")
+if [ $? -eq 0 ]; then
+	past_entries=""
 
-	path=~/.tagebuch/$year_past/$month_past/$today_past
+	html_skeleton=""
 
-	# Only execute when there is a folder/file corresponding to the past date
-	if [ -d $path ]; then
-		today_heading_past=$(date --date="$past_date" "+%A, %d. %B %Y")
-		zenity --question --text="Tagebucheintrag vom $today_heading_past, lesen?" --ok-label="Ja" --cancel-label="Nein" --timeout=10
+	configure_html_skeleton() {
+		# Formats the html skeleton for a diary entry
+		# $1 formatted heading
+		# Heute, $tag, $date, vor...
+		title="Heute, $(date "+%A, %d. %B %Y"), vor..."
+		html_skeleton="<!DOCTYPE html>
+	<html>
+	  <head>
+		<title>$title</title>
+		<!-- weitere Kopfinformationen -->
+		<!-- Styles für <pre> -->
+		<link rel=\"stylesheet\" href=\"/home/philipp/.tagebuch/style.css\">
+	  </head>
+	  <body>
+		<h1>$title</h1><br /> 
+		$1
+	  </body>
+	</html>"
+	}
 
-		if [ $? -eq 0 ]; then
+	past_dates=( $(date --file=past_dates.txt +%F) )
+	ctr=0
+	for past_date in ${past_dates[@]}; do
+		today_past=$(date --date="$past_date" "+%F-%A")
+		month_past=$(date --date="$past_date" "+%m-%B")
+		year_past=$(date --date="$past_date" "+%Y")
+
+		path=~/.tagebuch/$year_past/$month_past/$today_past
+
+		# Only execute when there is a folder/file corresponding to the past date
+		if [ -d $path ]; then
+			today_heading_past=$(date --date="$past_date" "+%A, %d. %B %Y")
 			# Wildcard expansion doesn't work in script, so it has to be triggered beforehand.
 			# This is done by using an array and indexing the first field. There is exactly one, because one html-file was generated.
 			# Taken from
 			#	https://unix.stackexchange.com/questions/663257/bin-sh-wildcard-expansion-does-not-work-in-script
 			files=("$path/$today_past*.html") 
-			firefox --new-window ${files[0]}
+			past_entries="$past_entries $(python3 /home/philipp/.tagebuch/extract_html_body.py ${files[0]} $ctr "$today_heading_past")"
+			ctr=$((ctr + 1))
 		fi
-	fi
-done
+	done
 
-
+	configure_html_skeleton "$past_entries"
+	echo $html_skeleton > /tmp/past.html
+	firefox --new-window /tmp/past.html &
+fi
---
+++ extract_html_body.py
@@ -0,0 +1,35 @@
+from sys import argv
+from bs4 import BeautifulSoup
+# Extract the <body> of the submitted diary entry (html file)
+# argv[1]   diary entry as html file; body is extracted from this file
+# argv[2]   "which" past is parsed, s. past_dates.txt
+# argv[3]   formatted date for heading according to argv[2]
+
+with open(argv[1]) as fp:
+    soup = BeautifulSoup(fp, 'html.parser')
+
+# "which" past is submitted as second argument
+# craft heading according to chosen past
+period = int(argv[2])
+match period:
+    case 0:
+        heading = f"...vier Wochen, {argv[3]}"
+    case 1:
+        heading = f"...einem Jahr, {argv[3]}"
+
+# Format past adapted headings
+h2 = f"<h2>{heading}</h2>"
+# Extract description in pre-tag
+pre = soup.find('pre')
+pre = f"<pre>{str(pre.contents[0]).strip()}</pre>"
+# Extract images/fotos
+imgs = soup.find_all('img')
+imgs = (f"{str(img)}<br />" for img in imgs)
+
+# Join all html elements
+html_body = "".join((h2, pre, *imgs))
+print(html_body)
+
+# In case generated html should be saved formatted
+# soup = BeautifulSoup(html_body, 'html.parser')
+# print(soup.prettify())
