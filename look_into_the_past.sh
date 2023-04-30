15225b061a1243141c31731efba76e471fa50d2f
Philipp Rost <rost.philipp@protonmail.com>
2023-04-30 21:38:10 +0200

    Only ask user if there is at least one past entry

--- look_into_the_past.sh
+++ look_into_the_past.sh
@@ -2,57 +2,69 @@
 # Open diary entry of past dates.
 # Past dates are specified in past_dates.txt
 
+# sym-linked into /etc/cron.daily/ (while being in this directory, not from "outside")
 
-zenity --question --text="In die Vergangenheit blicken?" --ok-label="Ja" --cancel-label="Nein" --width=1000 --height=300 --timeout=10
+configure_html_skeleton() {
+	# Formats the html skeleton for a diary entry
+	# $1 formatted heading
+	# Heute, $tag, $date, vor...
+	title="Heute, $(date "+%A, %d. %B %Y"), vor..."
+	html_skeleton="<!DOCTYPE html>
+<html>
+  <head>
+	<title>$title</title>
+	<!-- weitere Kopfinformationen -->
+	<!-- Styles für <pre> -->
+	<link rel=\"stylesheet\" href=\"/home/philipp/.tagebuch/style.css\">
+  </head>
+  <body>
+	<h1>$title</h1><br /> 
+	$1
+  </body>
+</html>"
+}
 
-if [ $? -eq 0 ]; then
-	past_entries=""
+past_entries=""
+html_skeleton=""
 
-	html_skeleton=""
+asked=1 # Variable to control that user is asked once, default is 1="Failure"="true"
+answer=1 # save answer of zenity --question
 
-	configure_html_skeleton() {
-		# Formats the html skeleton for a diary entry
-		# $1 formatted heading
-		# Heute, $tag, $date, vor...
-		title="Heute, $(date "+%A, %d. %B %Y"), vor..."
-		html_skeleton="<!DOCTYPE html>
-	<html>
-	  <head>
-		<title>$title</title>
-		<!-- weitere Kopfinformationen -->
-		<!-- Styles für <pre> -->
-		<link rel=\"stylesheet\" href=\"/home/philipp/.tagebuch/style.css\">
-	  </head>
-	  <body>
-		<h1>$title</h1><br /> 
-		$1
-	  </body>
-	</html>"
-	}
+past_dates=( $(date --file=/home/philipp/.tagebuch/past_dates.txt +%F) )
+ctr=0
+for past_date in ${past_dates[@]}; do
+	today_past=$(date --date="$past_date" "+%d-%m-%Y-%A")
+	month_past=$(date --date="$past_date" "+%m-%B")
+	year_past=$(date --date="$past_date" "+%Y")
 
-	past_dates=( $(date --file=past_dates.txt +%F) )
-	ctr=0
-	for past_date in ${past_dates[@]}; do
-		today_past=$(date --date="$past_date" "+%d-%m-%Y-%A")
-		month_past=$(date --date="$past_date" "+%m-%B")
-		year_past=$(date --date="$past_date" "+%Y")
+	paths=(~/.tagebuch/$year_past/$month_past/$today_past*)
+	path=${paths[0]}
 
-		paths=(~/.tagebuch/$year_past/$month_past/$today_past*)
-		path=${paths[0]}
-
-		# Only execute when there is a folder/file corresponding to the past date
-		if [ -d "$path" ]; then
-			today_heading_past=$(date --date="$past_date" "+%A, %d. %B %Y")
-			# Wildcard expansion doesn't work in script, so it has to be triggered beforehand.
-			# This is done by using an array and indexing the first field. There is exactly one, because one html-file was generated.
-			# Taken from
-			#	https://unix.stackexchange.com/questions/663257/bin-sh-wildcard-expansion-does-not-work-in-script
-			files=("$path/$today_past*.html") 
-			past_entries="$past_entries $(python3 ~/.tagebuch/extract_html_body.py ${files[0]} $ctr "$today_heading_past")"
-			ctr=$((ctr + 1))
+	# Only execute if there is a folder/file corresponding to the past date
+	# ...and the user wants to look into the past
+	if [ -d "$path" ]; then
+		if [ $asked -eq 1 ]; then
+			zenity --question --text="In die Vergangenheit blicken?" --ok-label="Ja" --cancel-label="Nein" --width=1000 --height=300 --timeout=10
+			answer=$?
+			if [ $answer -eq 1 ]; then
+				break
+			fi
+			asked=0 # Set $asked to prevent repeating questions in case user wants to look into the past
 		fi
-	done
 
+		today_heading_past=$(date --date="$past_date" "+%A, %d. %B %Y")
+		# Wildcard expansion doesn't work in script, so it has to be triggered beforehand.
+		# This is done by using an array and indexing the first field. There is exactly one, because one html-file was generated.
+		# Taken from
+		#	https://unix.stackexchange.com/questions/663257/bin-sh-wildcard-expansion-does-not-work-in-script
+		files=("$path/$today_past*.html") 
+		past_entries="$past_entries $(python3 ~/.tagebuch/extract_html_body.py ${files[0]} $ctr "$today_heading_past")"
+		ctr=$((ctr + 1))
+	fi
+done
+
+# Only when user wants to look into the past
+if [ $answer -eq 0 ]; then
 	configure_html_skeleton "$past_entries"
 	echo $html_skeleton > /tmp/past.html
 	firefox --new-window /tmp/past.html &
