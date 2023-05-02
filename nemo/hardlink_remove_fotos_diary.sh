46fb0f61ac70634a92854ad08ad76f53f492842b
Philipp Rost <rost.philipp@protonmail.com>
2023-05-02 19:54:35 +0200

    Added: nemo dir for files related to nemo

---
+++ nemo/hardlink_remove_fotos_diary.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# nemo action for hard linking selected fotos into the apropiate folder of the diary
+# folder name is read from pipe ~/.tagebuch/transfer_date and written by ~/.tagebuch/make_new_entry.sh
+
+# read from pipe
+path=$(<~/.tagebuch/transfer_date)
+
+# If directory exists, hard link selected images into it
+# Not explicitly necessary but prevents accidentally hard linking when not writing a diary entry
+if [ -d "$path" ]; then
+	for img in "$@"; do
+		ln "$img" "$path"
+        rm "$img"
+	done
+
+	nemo --quit
+fi
