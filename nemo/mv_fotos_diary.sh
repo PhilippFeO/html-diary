38fbef1f8eaff310cb8bdeb10506cd003861e4e3
Philipp Rost <rost.philipp@protonmail.com>
2023-05-03 22:55:46 +0200

    Tweaked: move (mv) fotos instead of hard linking (ln) and then removing (rm)

--- nemo/mv_fotos_diary.sh
+++ nemo/mv_fotos_diary.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-# nemo action for hard linking selected fotos into the apropiate folder of the diary
+# nemo action for moving fotos into the apropiate folder of the diary, i.e. fotos are removed from their original location
 # folder name is read from pipe ~/.tagebuch/transfer_date and written by ~/.tagebuch/make_new_entry.sh
 
 # read from pipe
 path=$(<~/.tagebuch/transfer_date)
 
-# If directory exists, hard link selected images into it
-# Not explicitly necessary but prevents accidentally hard linking when not writing a diary entry
-if [ -d "$path" ]; then
+# If directory exists, move files into it, i.e. files are "deleted" from their source folder
+if [ -d "$path" ]; then # "if" not explicitly necessary but prevents accidentally hard linking when not writing a diary entry
 	for img in "$@"; do
-		ln "$img" "$path"
-        rm "$img"
+		mv "$img" "$path"
 	done
 
 	nemo --quit
