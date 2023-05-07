270bd7c6e4a029b6bce015eca75147e2a0d5fb89
Philipp Rost <rost.philipp@protonmail.com>
2023-05-07 15:49:49 +0200

    Added: Include videos

--- extract_html_body.py
+++ extract_html_body.py
@@ -1,11 +1,13 @@
-from sys import argv
-from bs4 import BeautifulSoup
 # Extract the <body> of the submitted diary entry (html file)
 # argv[1]   diary entry as html file; body is extracted from this file
 # argv[2]   index of (line) "which" past is parsed, s. past_dates.txt
 # argv[3]   formatted date for heading according to argv[2]
 
 # TODO: Add "vor 6 Monaten" <29-04-2023>
+
+
+from sys import argv
+from bs4 import BeautifulSoup
 
 with open(argv[1]) as fp:
     soup = BeautifulSoup(fp, 'html.parser')
@@ -26,10 +28,13 @@ pre = soup.find('pre')
 pre = f"<pre>{str(pre.contents[0]).strip()}</pre>"
 # Extract images/fotos
 imgs = soup.find_all('img')
-imgs = (f"{str(img)}<br />" for img in imgs)
+imgs = (f"{str(img)}\n<br/>" for img in imgs)
+# Extract videos
+videos = soup.find_all('video')
+videos = (f"{str(video)}\n<br/>" for video in videos)
 
 # Join all html elements
-html_body = "".join((h2, pre, *imgs))
+html_body = "".join((h2, pre, *imgs, *videos))
 print(html_body)
 
 # In case generated html should be saved formatted
