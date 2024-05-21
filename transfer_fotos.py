f0f86d577f06af70de49a1a146325f6e7c425f4e
P_FeO <125358469+PhilippFeO@users.noreply.github.com>
2024-05-21 17:27:54 +0200

    🚚 Move: Code in transfer_fotos.py into function
    Now, I can test it

--- transfer_fotos.py
+++ transfer_fotos.py
@@ -24,32 +24,36 @@ length = 20
 logging.info(f'{"-" * length} {datetime.datetime.today()} {"-" * length}')
 
 
-# Loop through files in the directory
-for f in os.listdir(tmp_dir):
-    # file_path = os.path.join(tmp_dir, f)
-    file_path = tmp_dir/f
-    if os.path.isfile(file_path):
-        # Extract creation date from EXIF data
-        exif_output = subprocess.check_output(["exif", "-t", "0x9003", "--ifd=EXIF", file_path])
-        exif_lines = exif_output.decode("utf-8").splitlines()
-        date_created = exif_lines[-1].split()[1]
-        year, month, day = date_created.split(":", 2)
+def transfer_fotos() -> None:
+    # Loop through files in the directory
+    for f in os.listdir(tmp_dir):
+        # file_path = os.path.join(tmp_dir, f)
+        file_path = tmp_dir/f
+        if os.path.isfile(file_path):
+            # Extract creation date from EXIF data
+            exif_output = subprocess.check_output(["exif", "-t", "0x9003", "--ifd=EXIF", file_path])
+            exif_lines = exif_output.decode("utf-8").splitlines()
+            date_created = exif_lines[-1].split()[1]
+            year, month, day = date_created.split(":", 2)
 
-        # Find matching directories
-        matching_dirs = glob.glob(f"{year}/{month}-*/{day}-*")
-        count = len(matching_dirs)
+            # Find matching directories
+            matching_dirs = glob.glob(f"{year}/{month}-*/{day}-*")
+            count = len(matching_dirs)
 
-        if count == 1:
-            # Transfer image to directory
-            target_dir = matching_dirs[0]
-            shutil.copy(file_path,
-                        tagebuch_dir/target_dir/file_path.name)
+            if count == 1:
+                # Transfer image to directory
+                target_dir = matching_dirs[0]
+                shutil.copy(file_path,
+                            tagebuch_dir/target_dir/file_path.name)
 
-            logging.info(f"Copied '{file_path}' to '{tagebuch_dir/target_dir/file_path.name}'")
-            directories.add(target_dir)
-            logging.info(f"Added '{target_dir}' to `directories`")
-        else:
-            logging.error(
-                f"Found {count} matching Directories obeying '{year}/{month}-*/{day}-*'. There should be exactly 1. The Directories are:\n{','.join(matching_dirs)}")
+                logging.info(f"Copied '{file_path}' to '{tagebuch_dir/target_dir/file_path.name}'")
+                directories.add(target_dir)
+                logging.info(f"Added '{target_dir}' to `directories`")
+            else:
+                logging.error(
+                    f"Found {count} matching Directories obeying '{year}/{month}-*/{day}-*'. There should be exactly 1. The Directories are:\n{','.join(matching_dirs)}")
 
-add_media_files(directories)
+
+if __name__ == "__main__":
+    transfer_fotos()
+    add_media_files(directories)
