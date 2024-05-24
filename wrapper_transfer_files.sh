091516fad1c340599386c592189bf7613da66575
P_FeO <125358469+PhilippFeO@users.noreply.github.com>
2024-05-24 18:33:43 +0200

    🚚 Rename: wrapper script

--- wrapper_transfer_files.sh
+++ wrapper_transfer_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This is a wrapper script for running 'transfer_files.py' and 'add_media_files.py' in a cronjob.
-
+# Necessary because I need the 'tagebuch' venv activated.
 
 source /home/philipp/.tagebuch/.venv/tagebuch/bin/activate
 python3 /home/philipp/.tagebuch/transfer_files.py
