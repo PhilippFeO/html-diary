c26b6960136385d116af1fef6e4e6c9c2f7f3eeb
P_FeO <125358469+PhilippFeO@users.noreply.github.com>
2024-05-25 12:22:14 +0200

    ✨ Feat: Extract Date of PANO*.jpg fotos
    Panorama images can't be handled with 'exif', ie. another 'fallback' is
    necessary: exiftool. Panorama images also don't have a specific 'date
    created' tag, so I use 'GPS Date Stamp'.

--- transfer_files.py
+++ transfer_files.py
@@ -32,17 +32,31 @@ def transfer_files(tmp_dir: Path,
         file_path = tmp_dir/f
         if os.path.isfile(file_path):
             # Extract creation date from EXIF data
+            exif_output: bytes = bytes()
+            date_created: str = ''
             match file_path.suffix:
                 case '.mp4' | '.MP4':
                     exif_output = subprocess.check_output(["exiftool", "-CreateDate", file_path])
-                    exif_output = exif_output.decode('utf-8')
                     # Retrieve date from following line:
                     # Create Date                     : 2023:05:12 13:58:16
-                    date_created = exif_output.split(' : ')[-1].split(' ')[0]
+                    date_created = exif_output.decode('utf-8').split(' : ')[-1].split(' ')[0]
                 case '.jpg' | '.jpeg' | '.png' | '.MP4' | '.JPG' | '.JPEG':
-                    exif_output = subprocess.check_output(["exif", "-t", "0x9003", "--ifd=EXIF", file_path])
-                    exif_lines = exif_output.decode("utf-8").splitlines()
-                    date_created = exif_lines[-1].split()[1]
+                    try:
+                        exif_output = subprocess.check_output(["exif", "-t", "0x9003", "--ifd=EXIF", file_path])
+                        exif_lines = exif_output.decode("utf-8").splitlines()
+                        date_created = exif_lines[-1].split()[1]
+                    except subprocess.CalledProcessError:
+                        logging.error(f"Foto '{file_path}' has no Tag with ID 0x9003 in it's exif data.")
+                    # PANO*.jpg
+                    if date_created is None:
+                        try:
+                            # Panorama images dont have anything like 'date created', hence I have to use 'GPS Date Stamp':
+                            # GPS Date Stamp                  : 2024:05:22
+                            exif_output = subprocess.check_output(["exiftool", "-GPSDateStamp", file_path])
+                            # In comparison to the mp4 usecase, exif_output has a trailing \n which has to be removed
+                            date_created = exif_output.decode('utf-8').rstrip().split(' : ')[-1]
+                        except subprocess.CalledProcessError:
+                            logging.error(f"Foto '{file_path}' has no 'GPSDateStamp' Tag in it's exif data.")
                 case _:
                     logging.error(f"Nothing done for new File Type: {file_path}")
                     continue
@@ -50,22 +64,22 @@ def transfer_files(tmp_dir: Path,
             year, month, day = date_created.split(":", 2)
 
             # Find matching directories
-            matching_dirs = glob.glob(f"{year}/{month}-*/{day}-*")
-            count = len(matching_dirs)
+            matching_dirs = glob.glob(f"{year}/{month}-*/{day}-{month}-{year}-*")
 
-            if count == 1:
+            if (count := len(matching_dirs)) == 1:
                 # Transfer image to directory
                 target_dir = matching_dirs[0]
                 shutil.copy(file_path,
                             tagebuch_dir/target_dir/file_path.name)
-
                 logging.info(f"Copied '{file_path}' to '{tagebuch_dir/target_dir/file_path.name}'")
+                shutil.move(file_path,
+                            (transfered_dir := tagebuch_dir/'.tmp/transfered'))
+                logging.info(f"Moved '{file_path}' to '{transfered_dir}'.")
                 directories.add(target_dir)
-                logging.info(f"Added '{target_dir}' to `directories`")
+                logging.info(f"Added '{target_dir}' to `directories`.")
             else:
                 logging.error(
                     f"Found {count} matching Directories obeying '{year}/{month}-*/{day}-*'. There should be exactly 1. The Directories are:\n{', '.join(matching_dirs)}")
-                raise Exception('Check Log-File')
     return directories
 
 
