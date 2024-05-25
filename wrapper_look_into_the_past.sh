67bc435d10c86f71058f1ec93b6b811494b970da
P_FeO <125358469+PhilippFeO@users.noreply.github.com>
2024-05-25 12:20:41 +0200

    ✨ Feat: Move firefox window with summary
    Moves the opened firefox to another desktop window with the summary of the past days.

--- wrapper_look_into_the_past.sh
+++ wrapper_look_into_the_past.sh
@@ -9,3 +9,9 @@ export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 source /home/philipp/.tagebuch/.venv/tagebuch/bin/activate
 python3 /home/philipp/.tagebuch/look_into_the_past.py
 deactivate
+
+sleep 1
+
+# Firefox mit Tagebucheintrag der vergangenen Jahre
+diary_winID=$(wmctrl -l | grep "Heute," | grep -v grep | cut -d ' ' -f 1)
+wmctrl -i -r $diary_winID -t 4
