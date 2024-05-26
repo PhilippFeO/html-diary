9235bec23f3da4577c868031a92caae97be5d60e
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-05-26 16:47:03 +0200

    🚀 Improve: Logging in entry_for_past_day.py

--- entry_for_past_day.py
+++ entry_for_past_day.py
@@ -8,19 +8,6 @@ from bs4 import BeautifulSoup, Tag
 
 from vars import tagebuch_dir
 
-# TODO: in ifmain <26-05-2024>
-# Define the path to the directory containing the files
-tmp_dir = tagebuch_dir/'.tmp'
-
-logging.basicConfig(level=logging.INFO,
-                    format='[%(levelname)s: %(asctime)s] %(message)s',
-                    # Mit Datum: %d.%m.%Y
-                    datefmt=' %Y.%m.%d  %H:%M:%S',
-                    filename=tagebuch_dir/'.logs/entry_for_past_day.log.txt',
-                    filemode='a')
-length = 20
-logging.info(f'{"-" * length} {datetime.datetime.today()} {"-" * length}')
-
 
 def entry_for_past_day(date: str):
     date_obj = datetime.datetime.strptime(date, '%Y-%m-%d').date()
@@ -35,11 +22,13 @@ def entry_for_past_day(date: str):
 
     match len(day_dirs):
         case 0:  # No day-dir exists yet
+            logging.info(f'No directory for {date} exists.')
             x = input("Überschrift: ")
             title = f'{weekday}, {date}: {x}'
             x = x.replace(' ', '-')
             day_dir = day_dir_pattern.replace('*', x)
             os.makedirs(day_dir)
+            logging.info(f'Directory for {date} created: {day_dir}')
             html_skeleton = "<!DOCTYPE html>" + \
                 '<html>' + \
                 '  <head>' + \
@@ -59,18 +48,20 @@ def entry_for_past_day(date: str):
                 raise Exception('The found pre-tag is not of type Tag')
             description = input(f'Enter description for {date}:\n')
             pre_tag.string = description
-            with open(f'{day_dir}/{day}-{month}-{year}-{weekday}-{x}.html', 'w') as f:
+            with open((html_entry := f'{day_dir}/{day}-{month}-{year}-{weekday}-{x}.html'), 'w') as f:
                 f.write(entry_soup.prettify())
+            logging.info(f'New Entry created: {html_entry}')
         # day-dir already exists and hence an entry file
         # transfer_files() can create no-description entries which contain media files only
         case 1:
+            logging.info(f'Directory for {date} already exists: {day_dirs[0]}')
             html_files = glob.glob(f'{day_dir_pattern}/*.html')
             match len(html_files):
                 case 0:  # There is a day-dir but no HTML entry -> Can't happen currently
                     raise Exception(f'"{day_dirs[0]} doesn\'t contain a HTML file')
                 case 1:
                     html_file = html_files[0]
-                    logging.info(f'HTML file in "{day_dirs[0]}" present: {html_file}')
+                    logging.info(f'Entry for {date} exists: {html_file}')
                     with open(html_file, 'r') as f:
                         html = f.read()
                     if not html:
@@ -81,12 +72,16 @@ def entry_for_past_day(date: str):
                         case 0:
                             raise Exception(f'File "{html_file}" doesn\'t contain a <pre> tag')
                         case 1:
-                            # Query user for input
-                            description = input(f'Enter description for {date}:\n')
                             pre_tag = pre[0]
-                            pre_tag.string = description
-                            with open(html_file, 'w') as f:
-                                f.write(entry_soup.prettify())
+                            if pre_tag.string:
+                                raise Exception(f'Description for {date} exists: "{html_file}"')
+                            else:
+                                # Query user for input
+                                description = input(f'Enter description for {date}:\n')
+                                pre_tag.string = description
+                                with open(html_file, 'w') as f:
+                                    f.write(entry_soup.prettify())
+                                logging.info(f'Description for Date {date} added to File "{html_file}"')
                         # Too many pre-tags in HTML file
                         case _:
                             raise Exception(f'"{len(pre)} instead of 1 pre-tags in "{html_file}"')
@@ -96,11 +91,24 @@ def entry_for_past_day(date: str):
                     raise Exception(f'There are too many HTML files in "{day_dirs}":\n{html_files_str}')
         # To many day_dirs
         case _:
-            logging.warning(
+            raise Exception(
                 f"Found {len(day_dirs)} matching Directories obeying '{day_dir_pattern}'. There should be exactly 1. The Directories are:\n{', '.join(day_dirs)}")
 
 
 if __name__ == "__main__":
     import locale
     locale.setlocale(locale.LC_ALL, '')
+    # TODO: in ifmain <26-05-2024>
+    # Define the path to the directory containing the files
+    tmp_dir = tagebuch_dir/'.tmp'
+
+    logging.basicConfig(level=logging.INFO,
+                        format='[%(levelname)s: %(asctime)s] %(message)s',
+                        # Mit Datum: %d.%m.%Y
+                        datefmt=' %Y.%m.%d  %H:%M:%S',
+                        filename=tagebuch_dir/'.logs/entry_for_past_day.log.txt',
+                        filemode='a')
+    length = 20
+    logging.info(f'{"-" * length} {datetime.datetime.today()} {"-" * length}')
+
     entry_for_past_day(sys.argv[1])
