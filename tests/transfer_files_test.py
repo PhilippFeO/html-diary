9b62eb6758a2675b99690de25976bd0ba8f761ac
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-05-28 09:33:13 +0200

    🚚 Move(test): transerfer_files fixtures into transfer_files_test.py

--- tests/transfer_files_test.py
+++ tests/transfer_files_test.py
@@ -1,15 +1,17 @@
 import glob
+import shutil
+import pytest
 import os
 import subprocess
 from typing import TYPE_CHECKING
 
-from tests.fixtures_transfer_files import copy_fotos_tmp_dir, create_second_day_dir
 from tests.vars import (
     foto_1_name,
     foto_2_name,
     tf_foto_no_day_dir,
     tf_foto_two_day_dir,
     video_1_name,
+    tests_dir,
     test_diary_dir,
     test_tmp_dir,
     test_transfered_dir,
@@ -20,6 +22,33 @@ if TYPE_CHECKING:
     from pathlib import Path
 
 
+# ─── Fixtures ──────────
+
+@pytest.fixture
+def copy_fotos_tmp_dir(request):
+    # Get the parameter for the current test function
+    param_map: dict[str, tuple[Path, ...]] = {
+        'test_transfer_files': (tests_dir / foto_1_name,
+                                tests_dir / foto_2_name,
+                                tests_dir / video_1_name),
+        'test_transfer_files_no_day_dir': (tests_dir / tf_foto_no_day_dir,),
+        'test_transfer_files_two_day_dir': (tests_dir /
+                                            tf_foto_two_day_dir,)
+    }
+    param = param_map[request.node.name]
+    for media_file in param:
+        shutil.copy(media_file, test_tmp_dir)
+
+
+@pytest.fixture
+def create_second_day_dir():
+    day_dir2: Path = test_diary_dir/'2020/09-September/13-09-2020-Nicht-Bamberg'
+    os.makedirs(day_dir2)
+    yield
+    shutil.rmtree(day_dir2)
+
+
+# ─── Tests ──────────
 def test_transfer_files(setup_diary, copy_fotos_tmp_dir):
     """Tests if one foto is copied correctly from `tmp_dir` to it's corresponding 'day_dir_{fotos, video}'. This directory must exists for `transfer_files()` to work properly."""
     _ = setup_diary
