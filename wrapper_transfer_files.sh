b93f7faaea04236aad39420df1e0abe5207f9477
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-06-08 13:57:18 +0200

    🚀 Improve: cd into Tagebuch dir at first

--- wrapper_transfer_files.sh
+++ wrapper_transfer_files.sh
@@ -2,6 +2,7 @@
 # This is a wrapper script for running 'transfer_files.py' and 'add_media_files.py' in a cronjob.
 # Necessary because I need the 'tagebuch' venv activated.
 
+cd /home/philipp/.tagebuch
 source /home/philipp/.tagebuch/.venv/tagebuch/bin/activate
 python3 /home/philipp/.tagebuch/transfer_files.py
 deactivate
