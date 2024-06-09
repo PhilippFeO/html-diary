b6a783df92d5d494f5afe5e9f6f98592cfa6344c
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-06-09 13:19:58 +0200

    🐛 Fix(test): add_media_files_dir_file works now with tags as return value
    No BeautifulSoup is returned anymore but a list containing tags.

--- tests/add_media_files_test.py
+++ tests/add_media_files_test.py
@@ -7,17 +7,9 @@ def test_add_media_files_dir_file(setup_diary):
     diary_html = test_diary_dir/'2024/06-Juni/05-06-2024-Mittwoch-Test-add_media_files_day_dir/05-06-2024-Mittwoch-Test-add_media_files_day_dir.html'
     not_existent_foto_dir = test_diary_dir/'lorem_ipsum/'
 
-    soup = add_media_files_dir_file(diary_html, not_existent_foto_dir)
+    tags = add_media_files_dir_file(diary_html, not_existent_foto_dir)
 
-    # TODO: Use CLI arg for pytest to trigger this block <27-05-2024>
-    # html_path = Path('/tmp/add_media_files_dir_file.html')
-    # with open(html_path, 'w') as html_file:
-    #     html_file.write(soup.prettify())
-    # import subprocess
-    # subprocess.run(['firefox', html_path])
-
-    b_tag = soup.find('b')
-    if b_tag and b_tag.string:
-        assert f'{not_existent_foto_dir} EXISTIERT NICHT' in b_tag.string
-    else:
-        assert False, '`b_tag` or `b_tag.string` is False'
+    assert len(tags) == 2
+    assert tags[0].name == 'b'
+    assert tags[0].string == f'{not_existent_foto_dir} EXISTIERT NICHT'
+    assert tags[1].name == 'br'
