6a4b18474ec35b4d5a228ba4971479b1851a6274
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-06-09 14:17:41 +0200

    🐛 Fix: Reenable shadowed variable for tests

--- my_html_handler.py
+++ my_html_handler.py
@@ -21,24 +21,25 @@ logging.info(f'{"-" * length} {datetime.datetime.today()} {"-" * length}')
 
 
 def helper(html_file, dir_path):
-    soup = add_media_files_dir_file(html_file, Path(dir_path))
+    """Add media files in dir_path to diary entry"""
+    tags = add_media_files_dir_file(html_file, Path(dir_path))
     logging.info('Media files added')
-    with tempfile.NamedTemporaryFile(delete=False) as html_file:
-        html_file.write(bytes(soup.encode('utf-8')))
-        html_file.flush()
-        logging.info(f'Open {html_file.name}')
-        html_file = html_file.name
+    with open(html_file, 'r') as file:
+        html_content = file.read()
+    soup = BeautifulSoup(html_content, 'html.parser')
+    if (pre_tag := soup.find('pre')):
+        pre_tag.insert_after(*tags)
+    with tempfile.NamedTemporaryFile(delete=False) as html_tmp_file:
+        html_tmp_file.write(bytes(soup.prettify(encoding='utf-8')))
+        html_tmp_file.flush()
+        logging.info(f'Open {html_tmp_file.name}')
+        html_file = html_tmp_file.name
     subprocess.run(['open', html_file])
 
 
 def read_base_href(html_file):
-    logging.info(f"open_file('{html_file}')")
+    logging.info(f"read_base_href('{html_file}')")
     if '.tagebuch' in html_file:
-        # look_into_the_past.py calls the function without 'file://'
-        # 'file://' is only part of param when called via the nemo action
-        # Remove file:// from html_file
-        len_prefix = len(prefix) if (prefix := 'file://') in html_file else 0
-        html_file = html_file[len_prefix:]
         # Retrieve head.base.href attribute to embed images
         with open(html_file, 'r') as file:
             html_content = file.read()
@@ -47,9 +48,8 @@ def read_base_href(html_file):
         if soup.head and soup.head.base and (href := soup.head.base.get('href')):
             if isinstance(href, str):
                 # Remove file:// from base.href
-                dir_path = href[len(prefix):].replace('~', str(Path.home()))
+                dir_path = href[len('file://'):].replace('~', str(Path.home()))
                 logging.info(f'{dir_path = }')
-                # Add media files in dir_path to diary entry
                 return dir_path
             else:
                 logging.error('`href` is not of type `str` but `list[str] | None`.')
@@ -59,5 +59,8 @@ def read_base_href(html_file):
 
 
 if __name__ == "__main__":
-    dir_path = read_base_href((html_file := sys.argv[1]))
+    html_file = sys.argv[1]
+    prefix = 'file://'
+    html_file = html_file[len(prefix):]
+    dir_path = read_base_href(html_file)
     helper(html_file, dir_path)
