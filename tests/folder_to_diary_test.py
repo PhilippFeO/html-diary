6c2c24e6da40e12974e67ff2ca87a63f4b039726
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-06-16 23:31:46 +0200

    🚧 Wip(test/ftp)

--- tests/folder_to_diary_test.py
+++ tests/folder_to_diary_test.py
@@ -1,14 +1,13 @@
 from pathlib import Path
 
 from bs4 import BeautifulSoup
-
 from folder_to_diary import collect_dates_paths, folder_to_diary
-from tests.vars import test_diary_dir, tests_dir
+from tests.vars import TEST_BILDER_DIR, test_diary_dir
 
 
 def test_collect_dates():
-    dates_expected = ('2024:05:29', '2024:05:30', '2024:05:31')
-    directory = Path(tests_dir / 'Location 2024-05')
+    dates_expected = ('2024:05:29', '2024:05:30', '2024:05:31', '2024:06:01',  '2024:06:02')
+    directory = Path(TEST_BILDER_DIR / 'ftp-collect_dates_paths 2024-05')
     dates_paths_expected = {(date, directory) for date in dates_expected}
 
     dates_paths = collect_dates_paths(directory)
@@ -18,27 +17,27 @@ def test_collect_dates():
 
 def test_ftd_add_base_href():
     """Part of 'case 1': if."""
-    expected_path = Path(tests_dir / 'Location 2024-05')
+    expected_path = Path(TEST_BILDER_DIR / 'ftd-add_base_ref 2024-05')
 
     folder_to_diary(expected_path)
 
-    location_entry = test_diary_dir / '2024/05-Mai/30-05-2024-Donnerstag-base-href-hinzugefuegt-ftd/30-05-2024-Donnerstag-base-href-hinzugefuegt-ftd.html'
+    location_entry = test_diary_dir / '2024/05-Mai/30-05-2024-Donnerstag-ftp-add-base-ref/30-05-2024-Donnerstag-ftp-add-base-ref.html'
 
     entry = BeautifulSoup(location_entry.read_text(), 'html.parser')
 
     assert entry.head
     assert entry.head.base
-    assert entry.head.base.get('href') == 'file://' + str(expected_path)
+    assert entry.head.base.get('href') == f'file://{expected_path}'
 
 
-def test_ftd_href_differs(caplog):
+def test_ftd_href_present(caplog):
     """Part of 'case 1': else."""
-    dir_path = Path(tests_dir / 'Location 2024-05')
+    dir_path = Path(TEST_BILDER_DIR / 'ftd-href_present 2024-05')
 
     folder_to_diary(dir_path)
 
-    href_value = 'file:///home/philipp/test/test_tagebuch/Location 2024-05/'
-    entry_file = test_diary_dir / '2024/05-Mai/31-05-2024-Mittwoch-base-href-schon-vorhanden-ftd/31-05-2024-Mittwoch-base-href-schon-vorhanden-ftd.html'
+    href_value = 'file:///home/philipp/.tagebuch/tests/Bilder/ftd-href_present 2024-05/'
+    entry_file = test_diary_dir / '2024/05-Mai/31-05-2024-Mittwoch-ftd-href-present/31-05-2024-Mittwoch-ftp-href-present.html'
     href = f'file://{dir_path}'
 
     msg_expected = f"base.href is '{href_value}' in '{entry_file}'. Can't add '{href}'."
@@ -47,7 +46,7 @@ def test_ftd_href_differs(caplog):
 
 def test_ftd_create_new_entry():
     """Tests 'case 0'."""
-    foto_dir = Path(tests_dir / 'ftd-create_new_entry 2024-05')
+    foto_dir = Path(TEST_BILDER_DIR / 'ftd-create_new_entry 2024-05')
 
     folder_to_diary(foto_dir)
 
