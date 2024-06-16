dd860b512e68eb2cf35dd55dac0db6a1ae44eeec
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-06-16 16:55:54 +0200

    🚚 Move(test): folder name

--- tests/folder_to_diary_test.py
+++ tests/folder_to_diary_test.py
@@ -7,7 +7,7 @@ from tests.vars import test_diary_dir, tests_dir
 
 def test_collect_dates():
     dates_expected = ('2024:05:29', '2024:05:30', '2024:05:31', '2024:06:01', '2024:06:02')
-    directory = Path(tests_dir / 'Bibione 2024-05')
+    directory = Path(tests_dir / 'Location 2024-05')
     dates_paths_expected = {(date, directory) for date in dates_expected}
 
     dates_paths = collect_dates_paths(directory)
@@ -16,14 +16,14 @@ def test_collect_dates():
 
 
 def test_folder_to_diary():
-    bibione_fotos = Path(tests_dir / 'Bibione 2024-05')
+    expected_path = Path(tests_dir / 'Location 2024-05')
 
-    folder_to_diary(bibione_fotos)
+    folder_to_diary(expected_path)
 
-    bibione1 = test_diary_dir / '2024/05-Mai/30-05-2024-Donnerstag-base-href-hinzugefuegt-ftd/30-05-2024-Donnerstag-base-href-hinzugefuegt-ftd.html'
+    location_root_dir = test_diary_dir / '2024/05-Mai/30-05-2024-Donnerstag-base-href-hinzugefuegt-ftd/30-05-2024-Donnerstag-base-href-hinzugefuegt-ftd.html'
 
-    entry = BeautifulSoup(bibione1.read_text(), 'html.parser')
+    entry = BeautifulSoup(location_root_dir.read_text(), 'html.parser')
 
     assert entry.head
     assert entry.head.base
-    assert entry.head.base.get('href') == 'file://' + str(bibione_fotos)
+    assert entry.head.base.get('href') == 'file://' + str(expected_path)
