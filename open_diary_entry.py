0c2ccc32b874f3eaf09b49d60fb84371a57a24fe
PhilippFeO <125358469+PhilippFeO@users.noreply.github.com>
2024-09-24 17:22:28 +0200

    feat: ✨ Refactor with 'Entry'

--- open_diary_entry.py
+++ open_diary_entry.py
@@ -2,24 +2,26 @@ import logging
 import subprocess
 import tempfile
 from pathlib import Path
-
-from bs4 import BeautifulSoup, Tag
+from typing import TYPE_CHECKING
 
 import vars
 from add_media_files import create_tags
+from entry import Entry
+
+if TYPE_CHECKING:
+    from bs4 import Tag
 
 
-def helper(html_file: Path, media_dir: Path) -> str:
+def helper(entry: Entry) -> str:
     """Add media files in `media_dir` to diary entry."""
-    logging.info("Args: html_file = %s, media_dir = %s", html_file, media_dir)
-    tags: list[Tag] = create_tags(html_file, media_dir)
+    logging.info("Args: html_file = %s, media_dir = %s", entry, entry.base_href)
+    assert entry.base_href is not None  # Alawys True because function isn't called when entry.base_href == False
+    tags: list[Tag] = create_tags(entry, entry.base_href)
     logging.info("Insert created tags after '<pre>'")
-    html_content = Path(html_file).read_text(encoding='utf-8')
-    soup = BeautifulSoup(html_content, 'html.parser')
-    if (pre_tag := soup.find('pre')):
+    if (pre_tag := entry.soup.find('pre')):
         pre_tag.insert_after(*tags)
     with tempfile.NamedTemporaryFile(delete=False) as html_tmp_file:
-        html_tmp_file.write(bytes(soup.prettify(encoding='utf-8')))
+        html_tmp_file.write(bytes(entry.soup.prettify(encoding='utf-8')))
         html_tmp_file.flush()
         return html_tmp_file.name
 
@@ -33,9 +35,10 @@ if __name__ == "__main__":
     logging.info('%s %s %s', vars.HLINE, 'open_diary_entry.py', vars.HLINE)
     import sys
     import urllib.parse
-    html_file = urllib.parse.unquote(sys.argv[1])
+    file = urllib.parse.unquote(sys.argv[1])
     prefix = 'file://'
-    html_file = Path(html_file[len(prefix):])
-    if (dir_path := read_base_href(html_file)):
-        html_file = helper(html_file, dir_path)
-    subprocess.run(['/usr/bin/firefox', html_file], check=False)
+    file = Path(file[len(prefix):])
+    entry = Entry(path_to_parent_dir=file.parent)
+    if entry.base_href is not None:
+        file = helper(entry)
+    subprocess.run(['/usr/bin/firefox', file], check=False)
